6a55fd11c993c718a88ab8337c0a5409e9d432c6
Gabriel Falcão <gabriel@nacaolivre.org>
2024-04-09 19:55:44 +0000

    U9fVNRPqSJLNN37DB7bQZEvR+m7UnlBgcS/FKooEZ0Hey5XDOXHWuAtzBNEFoNu9P0ZdzUF0tsRIAyghgHrVoCNqIClCTEw9zSxmer3+j2F+8rsaV44giSvRtSSpPP55cLb9j95xITZRKQgtltgSbNLZcAnBn3n9bNWk0PkyOSRWkzZXGSfxODXfZPQiToBHPjVbW7DgQKTHt4UJP7dWt4dQmvfaAX3DqjVGjzhQWXWIAJhTgN6Q+u0FTgWwCZglVCHIfz+/Dg/+4B7uo+/wJMLUJLKS8/CGGFlCeOz2gmO7nrkrk45zazI0xYz+MHaydJg05+eyVrF6lsBrMkpiIazxkGM9CVGw27y67fs3Sfp8qjgyyaAo/DBrzsIBgtHNNN==
    U9fVNRPqSJLNN4AYmH6ZYgMY9DVNVwk8nDxNNNN=

---
+++ 1-package-management.el
@@ -0,0 +1,31 @@
+;; Setup package sources as described in MELPA’s Getting Started Guide.
+
+(require 'package)
+
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+
+
+(when (require 'use-package nil 'noerror)
+  (eval-when-compile
+    ;; Following line is not needed if use-package.el is in ~/.emacs.d
+    (package-refresh-contents)
+    (package-install 'use-package)
+    (require 'use-package)))
+
+
+;; ;; I prefer installing packages with use-package since it makes easier
+;; ;; to organize code by grouping a package’s installation with it’s
+;; ;; configuration.
+;; (unless (package-installed-p 'use-package)
+;;   )
+
+
+(setq use-package-always-ensure t)
---
+++ init.el
@@ -0,0 +1,18 @@
+;; Keep emacs Custom-settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(defun load-file-if-exists (file-name)
+  (let ((file-path (expand-file-name file-name user-emacs-directory)))
+    (load-file file-path)
+    ))
+
+
+(load-file-if-exists "typst-mode.el")
+(load-file-if-exists "0-basics.el")
+(load-file-if-exists "1-package-management.el")
+(load-file-if-exists "2-shell.el")
+(load-file-if-exists "3-autocomplete.el")
+(load-file-if-exists "4-theme.el")
+(load-file-if-exists "5-mode-bindings.el")
+(load-file-if-exists "6-hooks.el")
+(load-file-if-exists "7-programming-tools.el")
+;;(load-file "custom.el")
