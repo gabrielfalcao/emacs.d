0cf396f450dfa41a9bc58b5a14133998045074ad
Gabriel Falcão <gabriel@nacaolivre.org>
2024-06-09 21:29:54 +0000

    07e8151d360906

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 ;; OO        -- gg  gg  gg gggggggg gggggggg gggggggg
 ;; OOOOOOOOOOOG
 (setq mode-line-format (list (base64-decode-string "SGkgbWFzdGVyIGdhYnJpZWxmYWxjYW8gcGxlYXNlIHdhaXQuLi4=")))
+(setq server-socket-dir "~/.emacs.d/socket" server-log t)
 (let ((foreground "#A79C83")
       (background "#333"))
   (set-face-attribute 'default nil :foreground foreground :background background :font "JetBrains Mono-20")
