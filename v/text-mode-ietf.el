4d3a54a9cccfb8eb193fdd14c48d97bfb3c41d1a
Gabriel Falcão <gabrielteratos@gmail.com>
2025-04-17 01:58:49 +0000

    cleanup

--- v/text-mode-ietf.el
+++
@@ -1,6 +0,0 @@
-;;
-;; ;; ;; ;; (defface ietf-txt-toc-title '((t :inherit font-lock-string-face))
-;; ;; ;; ;;   "Table of Contents"
-;; ;; ;; ;;   :group 'text-mode
-;; ;; ;; ;;   :version "22.1")
-;;
