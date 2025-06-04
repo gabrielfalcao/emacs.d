f039db616237f9d30b277fa515d4c922458a85d3
Gabriel Falcão <gabrielteratos@gmail.com>
2025-06-04 22:09:35 +0000

    functions.el
    hooks.el (2025-06-04 22:09:35)

--- c/hooks.el
+++ c/hooks.el
@@ -29,5 +29,6 @@
               (let* ((full-path (expand-file-name (buffer-file-name)))
                      (parent-path (file-name-directory (full-path)))
                      (filename (file-name-nondirectory (full-path))))
-                (setq buffer-auto-save-file-name (file-name-concat parent-path (format ".%s.emacs-auto-save" filename)))
+                (progn
+                  (setq buffer-auto-save-file-name (file-name-concat parent-path (format ".%s.emacs-auto-save" filename))))
                 )))
