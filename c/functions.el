2146ec106b1867aa172d305ea18f2f2344dddb7d
Gabriel Falcão <gabrielteratos@gmail.com>
2025-06-04 00:33:39 +0000

    /Users/gabrielfalcao/.emacs.d/c/functions.el

--- c/functions.el
+++ c/functions.el
@@ -1363,7 +1363,7 @@
                         0))
          )
     (defun state ()
-      (format "{\n    point: %s\n    til-next-open: %s,\n    til-next-close: %s,\n    count: %s,\n    count: %s\n}" (point) til-next-open til-next-close count count)
+      (format "{\n    point: %s\n    til-next-open: %s,\n    til-next-close: %s,\n    open-count: %s,\n    close-count: %s\n}" (point) til-next-open til-next-close open-count close-count)
       )
 
     (message (state))
