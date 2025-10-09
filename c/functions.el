gabrielfalcao/emacs.d

commit 4c75c1a26cb672a784b6ff492fd8cab7b631a8f1

refactors '(local|global)-(set|unset)[-]key\b' 'keymap-${1}-${2}' -wp .

c/functions.el
@@ -231,8 +231,8 @@
           (consp key)
           (listp key)))
     (user-error "key %S has invalid type: %s" key (type-of key)))
-  (when (integerp key) (global-unset-key key))
-  (when (stringp key) (global-unset-key (kbd key)))
+  (when (integerp key) (keymap-global-unset key))
+  (when (stringp key) (keymap-global-unset (kbd key)))
 
   (when (or (consp key) (vectorp key)
             ;;            (arrayp key)
@@ -252,10 +252,10 @@
     (user-error "key %S has invalid type: %s" key (type-of key)))
 
   (when (integerp key)
-    (progn (Ox33b4O/$/undefine-key key) (global-set-key key)))
+    (progn (Ox33b4O/$/undefine-key key) (keymap-global-set key)))
 
   (when (stringp key)
-    (progn (Ox33b4O/$/undefine-key key) (global-set-key (kbd key) def)))
+    (progn (Ox33b4O/$/undefine-key key) (keymap-global-set (kbd key) def)))
   (when (or (consp key)
             ;;            (arrayp key)
             (vectorp key)
@@ -275,9 +275,9 @@
           (consp key)
           (listp key)))
     (user-error "key %S has invalid type: %s" key (type-of key)))
-  (when (integerp key) (global-set-key key def))
+  (when (integerp key) (keymap-global-set key def))
 
-  (when (stringp key) (global-set-key (kbd key) def))
+  (when (stringp key) (keymap-global-set (kbd key) def))
   (when (or (consp key) (vectorp key)
             ;;            (arrayp key)
             (listp key))
@@ -3023,3 +3023,10 @@ BEG END."
       (goto-char beg)
       (delete-prefix-and-timestamp-from-bash-history-region beg end)
       )))
+
+;; (defun inspect-keymap-in-buffer (buffer)
+;;   "WIP ."
+;;   (if (not (bufferp buffer))
+;;       (user-error "not a buffer: %S" buffer))
+;;   (let ((buffer-local-keymap (buffer-local-value 'keymap buffer)))
+;;     (message (format "keymap for %s: %S" (buffer-name) buffer-local-keymap))))

c/hooks.el
@@ -67,8 +67,8 @@
               (interactive)
 
               (setq 'sh-basic-offset 4)
-              (local-unset-key (kbd "C-c C-f"))
-              (local-set-key (kbd "C-c C-f") #'shfmt)))
+              (keymap-local-unset (kbd "C-c C-f"))
+              (keymap-local-set (kbd "C-c C-f") #'shfmt)))
 
 (add-hook 'web-mode-hook
           #'(lambda ()
@@ -88,7 +88,7 @@
                (Ox33b4O/$/paint-mode-line nil "new buffer")))
 (add-hook 'toml-mode-hook #'(lambda ()
                               (setq fill-column 120)))
-;; (add-hook 'toml-mode-hook #'(lambda () ( (local-set-key (kbd '("C-c C-f") 'toml-prettify-buffer)))))
+;; (add-hook 'toml-mode-hook #'(lambda () ( (keymap-local-set (kbd '("C-c C-f") 'toml-prettify-buffer)))))
 ;; (add-hook 'local-write-file-hooks 'git-add-opt-libexec)
 
 (add-hook 'after-set-visited-file-name-hook

c/keys.el
@@ -75,7 +75,7 @@
 
 (Ox33b4O/$/set-key '("C-x M-k") 'Ox33b4O/$/load-library)(Ox33b4O/$/set-key '("C-x M-,") 'Ox33b4O/$/load-init)
 
-(global-set-key (kbd "C-c C-c C-r") 'collapse-lines-region)
+(keymap-global-set (kbd "C-c C-c C-r") 'collapse-lines-region)
 
 ;; (Ox33b4O/$/set-key '("M-, M-8" "M-, 8")#'(lambda () (interactive) (rot13(fpuervo) "OE8x"))) ;; https://deezer.page.link/HUJ1z8CfsudS2he96
 ;; (Ox33b4O/$/set-key '("M-, M-5" "M-, 5")#'(lambda () (interactive) (schreib "NU8x")))
@@ -147,7 +147,7 @@
 (Ox33b4O/$/set-key   "C-x C-j" #'(lambda () (interactive) (insert "\n")))
 
 (setq        ring-bell-function               'ignore)
-(global-set-key [kp-delete] 'delete-char)
+(keymap-global-set [kp-delete] 'delete-char)
 (setq select-enable-clipboard nil)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-screen t)

commit f56e097aa7ab25fd24490bc1245b3e73c93c1bea

ignore an error caused by changes in 4c75c1a26cb672a784b6ff492fd8cab7b631a8f1

c/functions.el
@@ -593,7 +593,7 @@
           "MHgwYzlmNjAwMCAtZCAtLSAnJXMn"))))))
 
 ;;(Ox33b4O/$/undefine-key (list "C-c C-e C-2" "C-c C-d C-2"))
-(progn
+(ignore-errors
   (Ox33b4O/$/set-key (list "C-c C-e C-2 C-0") 'Ox33b4O/$/encrypt-chacha20-hardcoded)
   (Ox33b4O/$/set-key
    (list "C-c C-e C-d C-2 C-0")