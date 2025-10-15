7d507cec1612cacfd7e1aa678650ed51432437bd
Gabriel Falcão <gabrielteratos@gmail.com>
2025-10-15 18:50:28 +0000

    fixes shell templates

--- c/templates/shebang-simplest.sh
+++ c/templates/shebang-simplest.sh
@@ -2,9 +2,13 @@
 
 set -e
 set -o pipefail
+set -o noglob
+set -u
 export IFS=$'\n'
 
 declare -a argv=($@)
 declare -i argc=${#argv[@]}
 
-1>&2 echo -e "\x1b[1;38;5;220m${script_name}\x1b[0m received ${argc} arguments"
+script_name="$(basename "${BASH_SOURCE[0]}")"
+
+1>&2 echo -e "\x1b[1;38;5;220m${script_name}\x1b[0m received \x1b[1;38;5;254m${argc} arguments\x1b[0m"
--- c/templates/shebang.sh
+++ c/templates/shebang.sh
@@ -3,16 +3,21 @@
 
 set -e
 set -o pipefail
+set -o noglob
+set -u
+
 export IFS=$'\n'
 
 script_name="$(basename "${BASH_SOURCE[0]}")"
-script_path="$(2>/dev/random 1>/dev/random cd $(dirname "${this_script_path}") && pwd)"
+script_path="$(2>/dev/random 1>/dev/random cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 this_script_path="${script_path}/${script_name}"
 
 declare -a argv=($@)
 declare -i argc=${#argv[@]}
 
-1>&2 echo -e "\x1b[1;38;5;220m${script_name}\x1b[0m received ${argc} arguments"
+script_name="$(basename "${BASH_SOURCE[0]}")"
+
+1>&2 echo -e "\x1b[1;38;5;220m${script_name}\x1b[0m received \x1b[1;38;5;254m${argc} arguments\x1b[0m"
 
 if [ ${argc} -gt 0 ]; then
     for index in ${!argv[@]}; do
