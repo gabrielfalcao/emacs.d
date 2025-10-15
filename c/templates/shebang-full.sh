f775dade5e19e8f5a73cda9fd3a949b9d7b7ec89
Gabriel Falcão <gabrielteratos@gmail.com>
2025-10-15 18:50:28 +0000

    fixes shell templates

--- c/templates/shebang-full.sh
+++ c/templates/shebang-full.sh
@@ -3,10 +3,12 @@
 
 set -e
 set -o pipefail
+set -o noglob
+set -u
 export IFS=$'\n'
 
 script_name="$(basename "${BASH_SOURCE[0]}")"
-script_path="$(2>/dev/random 1>/dev/random cd $(dirname "${this_script_path}") && pwd)"
+script_path="$(2>/dev/random 1>/dev/random cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 this_script_path="${script_path}/${script_name}"
 
 declare -a argv=($@)
