d1deee94c7099cd0c79bd7c9d5716397e847a325
Gabriel Falcão <gabrielteratos@gmail.com>
2025-10-21 16:33:32 +0000

    c/boot.el: add operating-system consts to be used in places like $font-name$ in c/ui.el

--- c/boot.el
+++ c/boot.el
@@ -10,13 +10,79 @@
   ;; (setq case-fold-search t)
   (defalias 'yes-or-no-p 'y-or-n-p)
   (defalias 'describe 'describe-symbol)
-  (defalias 'entry 'insert-timestamp)
-  (defalias 'elisp-mode 'emacs-lisp-mode)
   )
 (add-to-list 'custom-safe-themes "5bd001a0f95d54174370e9275b1f594829930a1a95ed82741a5492facb7415e7")
-(set-face-attribute 'default nil :font "JetBrains Mono-15")
+(set-face-attribute 'default nil :font "JetBrains Mono-13")
+
+;;
+;; given the region:
+;;
+;; ```emacs-lisp
+;; (kernel-name ;; Linux
+;;  (shell-command-to-string "uname -s"))
+;; (operating-system-name ;; GNU/Linux
+;;  (shell-command-to-string "uname -o"))
+;; (hardware-platform-name ;; x86_64
+;;  (shell-command-to-string "uname -m"))
+;; ;;```
+;;
+;; run `replace-regexp-in-region' with regexp input and replacement
+;;
+;; ```replace-regexp input
+;; ^(\(\([a-z-]+\)-name\)[;[:space:]]+\([^[:space:]]+\)[[:space:]]*\n+[[:space:]]*\((shell-command-to-string\s-+"\([^"]+\)")\))$
+;; ```
+;;
+;; ```replace-regexp replacement
+;; (defconst '\1 ;; \\1 => \1\n        \4 ;; init value \\4 => \4\n        "string with \2 name obtained via \\"\5\\" during emacs initialization"\n        ;; example value: \\"\2\\"\n)\n
+;; ```
+;;
+(defconst 'kernel-name ;; \1 => kernel-name
+        (shell-command-to-string "uname -s") ;; init value \4 => (shell-command-to-string "uname -s")
+        "string with kernel name obtained via \"uname -s\" during emacs initialization"
+        ;; example value: \"kernel\"
+)
+(defconst 'operating-system-name ;; \1 => operating-system-name
+        (shell-command-to-string "uname -o") ;; init value \4 => (shell-command-to-string "uname -o")
+        "string with operating-system name obtained via \"uname -o\" during emacs initialization"
+        ;; example value: \"operating-system\"
+)
+(defconst 'hardware-platform-name ;; \1 => hardware-platform-name
+        (shell-command-to-string "uname -m") ;; init value \4 => (shell-command-to-string "uname -m")
+        "string with hardware-platform name obtained via \"uname -m\" during emacs initialization"
+        ;; example value: \"hardware-platform\"
+)
+
+(defun font-name-for-system()
+  (let (
+        (default-font-name "JetBrains Mono")
+        (linux-font-size "13")
+        (osx-font-size "21")
+        (os-name-emacs-sysconf (format "%s" system-configuration))
+        (kernel-name ;; Linux
+         (shell-command-to-string "uname -s"))
+        (operating-system-name ;; GNU/Linux
+         (shell-command-to-string "uname -o"))
+        (hardware-platform-name ;; x86_64
+         (shell-command-to-string "uname -m"))
+        ) ;; end let variables
+    (cond
+     ((string= "Linux" kernel-name) ;; linux
+      (format "%s-%s" default-font-name linux-font-size))
+     ((string= "Darwin" kernel-name) ;; osx
+      (format "%s-%s" default-font-name osx-font-size))
+     (t ;; fallback when neither linux or darwin
+      (let ((full-font-spec (format "%s-21" )))
+        (message "(font-name-for-system) detected neither Linux nor MacOS but rather: kernel-name=%s and operating-system-name=%s"
+                 kernel-name operating-system-name)
+          full-font-spec))
+     );;end cond
+    );; end let
+    );; end defun
+
+
 (load-library "ui")
 (load-library "functions")
+(load-library "advices")
 (load-library "debug-et-diagnostics")
 (load-library "g-modeline")
 (load-library "keys")
@@ -47,3 +113,7 @@
 
 (setq history-length 1000)
 (setq history-delete-duplicates t)
+
+;; (set-window-buffer (split-window-right) "*Messages*")
+;; (erase-messages)
+;; (enable-debug-on-error)
