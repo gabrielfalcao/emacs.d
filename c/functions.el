21aab3a4d738f0f822459b3f4f92d7581f3f2320
Gabriel Falcão <gabrielteratos@gmail.com>
2025-12-17 13:09:46 +0000

    add "Ox33b4O/open-*-el" functions

--- c/functions.el
+++ c/functions.el
@@ -5727,3 +5727,70 @@ Margins::.
 "
   (interactive "*r")
   (add-text-properties beg end (list 'right-margin 10)))
+
+
+
+
+(defun Ox33b4O/open-boot-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/boot.el"))
+
+(defun Ox33b4O/open-ui-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/ui.el"))
+
+(defun Ox33b4O/open-e02491d9-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/e02491d9.el"))
+
+(defun Ox33b4O/open-modes-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/modes.el"))
+
+(defun Ox33b4O/open-debug-et-diagnostics-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/debug-et-diagnostics.el"))
+
+(defun Ox33b4O/open-keys-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/keys.el"))
+
+(defun Ox33b4O/open-hooks-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/hooks.el"))
+
+(defun Ox33b4O/open-advices-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/advices.el"))
+
+(defun Ox33b4O/open-functions-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/functions.el"))
+
+(defun Ox33b4O/open-flatten-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/flatten.el"))
+
+(defun Ox33b4O/open-g-modeline-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/g-modeline.el"))
+
+(defun Ox33b4O/open-other-functions-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/other-functions.el"))
+
+(defun Ox33b4O/open-elpamelpa-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/elpamelpa.el"))
+
+(defun Ox33b4O/open-rgb-parser-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/rgb-parser.el"))
+
+(defun Ox33b4O/open-write-refactor-tool-instead-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/write-refactor-tool-instead.el"))
+
+(defun Ox33b4O/open-macros-el()
+    (interactive)
+    (find-file-existing "~/.emacs.d/c/macros.el"))
