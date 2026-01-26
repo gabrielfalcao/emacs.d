33b2e40c78d1dbbdaf8c3563c6dcb010eb7214a8
Gabriel Falcão <gabrielteratos@gmail.com>
2026-01-26 20:45:05 +0000

    saves functions-wip-2026-01-26.17-07-02.1769458022.el

--- c/functions-wip/functions-wip-2026-01-26/functions-wip-2026-01-26.17-07-02.1769458022.el
+++ c/functions-wip/functions-wip-2026-01-26/functions-wip-2026-01-26.17-07-02.1769458022.el
@@ -4,5 +4,5 @@
 
 
 (defconst 'shell-script-varname-regexp-string
-  "\(^\(?:\(\s-*\)\(declare\|local\)\(\s-+[-][a-zA-Z-]+\s-*\)\)\|[^_a-zA-Z0-9_\x0a]\b\)\(\([a-zA-Z_]+\)\([a-zA-Z0-9_]*\)\)\($\|\b[^_\x0a]\)"
+  "\(^\(?:\(\s-*\)\(declare\|local\)\(\s-+[-][a-zA-Z-]+\s-*\)\)\|^\|[^_a-zA-Z0-9_\x0a]\b\)\(\([a-zA-Z_]+\)\([a-zA-Z0-9_]*\)\([[]\(?:[0-9]+\|\"[0-9]+\"\|'[0-9]+'\|[^]]+\|\"[^]]+\"\|'[^]]+'\)[]]\)?\)\(\b[^_\x0a]\|\(?:\(\s-*\)\(declare\|local\)\(\s-+[-][a-zA-Z-]+\s-*\)\)$\|$\)"
   "regular expression for matching shell-script varnames such as `${varname}' or `varname' but never `_varname_'")
