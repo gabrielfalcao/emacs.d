4d5080e72bbf5fca8ecd20d757cf17228824e835
Gabriel Falcão <gabrielteratos@gmail.com>
2026-01-26 20:27:31 +0000

    saves functions-wip-2026-01-26.17-07-02.1769458022.el

--- c/functions-wip/functions-wip-2026-01-26/functions-wip-2026-01-26.17-07-02.1769458022.el
+++ c/functions-wip/functions-wip-2026-01-26/functions-wip-2026-01-26.17-07-02.1769458022.el
@@ -1,3 +1,8 @@
-(defconst 'shell-script-varname-regexp
+(defconst 'shell-script-varname-regexp-read
   "\\(^\\|[^_]\\b\\)\\(\\([a-zA-Z_]\\)\\([a-zA-Z0-9_]*\\)\\)\\($\\|\\b[^_]\\)"
+  "regular expression for matching shell-script varnames such as `${varname}' or `varname' but never `_varname_'")
+
+
+(defconst 'shell-script-varname-regexp-string
+  "\(^\|[^_]\b\)\(\([a-zA-Z_]\)\([a-zA-Z0-9_]*\)\)\($\|\b[^_]\)"
   "regular expression for matching shell-script varnames such as `${varname}' or `varname' but never `_varname_'")
