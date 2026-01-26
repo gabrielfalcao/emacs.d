e969d6cb435eb43af834eb5faee18241fc17c2d3
Gabriel Falcão <gabrielteratos@gmail.com>
2026-01-26 20:46:54 +0000

    saves functions-wip-2026-01-26.17-07-02.1769458022.el

--- c/functions-wip/functions-wip-2026-01-26/functions-wip-2026-01-26.17-07-02.1769458022.el
+++ c/functions-wip/functions-wip-2026-01-26/functions-wip-2026-01-26.17-07-02.1769458022.el
@@ -1,5 +1,5 @@
 (defconst 'shell-script-varname-regexp-read
-  "\\(^\\|[^_]\\b\\)\\(\\([a-zA-Z_]\\)\\([a-zA-Z0-9_]*\\)\\)\\($\\|\\b[^_]\\)"
+  "\\(^\\(?:\\(\\s-*\\)\\(declare\\|local\\)\\(\\s-+[-][a-zA-Z-]+\\s-*\\)\\)\\|^\\|[^_a-zA-Z0-9_\x0a]\\b\\)\\(\\([a-zA-Z_]+\\)\\([a-zA-Z0-9_]*\\)\\([[]\\(?:[0-9]+\\|\"[0-9]+\"\\|'[0-9]+'\\|[^]]+\\|\"[^]]+\"\\|'[^]]+'\\)[]]\\)?\\)\\(\\b[^_\x0a]\\|\\(?:\\(\\s-*\\)\\(declare\\|local\\)\\(\\s-+[-][a-zA-Z-]+\\s-*\\)\\)$\\|$\\)"
   "regular expression for matching shell-script varnames such as `${varname}' or `varname' but never `_varname_'")
 
 
