c1d9f8cdcacca2fd5e2929a45b4ad874bdcd6ea7
Gabriel Falcão <gabrielteratos@gmail.com>
2026-01-27 00:24:11 +0000

    saves regexp-parens.el

--- c/regexp-parens.el
+++ c/regexp-parens.el
@@ -1,13 +1,14 @@
+;; OzsoZGVmdW4gZGlzcGxheS1saXN0IChjb250YWluZXItdmFsdWVzICZvcHRpb25hbCBkaXNwbGF5LWxldmVsIGNvbnRhaW5lci1uYW1lKQo7OyAgKHVubGVzcyAobGlzdHAgY29udGFpbmVyLXZhbHVlcykKOzsgICAgKHNpZ25hbCAndHlwZS1lcnJvcgo7OyAgICAgICAgICAgIChmb3JtYXQgImBjb250YWluZXItdmFsdWVzJyBzaG91bGQgYmUgYSBsaXN0IG5vdCAlczogJXMiCjs7ICAgICAgICAgICAgICAgICAgICAodHlwZS1vZiBjb250YWluZXItdmFsdWVzKQo7OyAgICAgICAgICAgICAgICAgICAgY29udGFpbmVyLXZhbHVlcykpKQo7OyAgKHdoZW4gKG51bGwgZGlzcGxheS1sZXZlbCkKOzsgICAgICAoc2V0cSBkaXNwbGF5LWxldmVsIDApKQo7Owo7OyAgKHdoZW4gKG51bGwgY29udGFpbmVyLW5hbWUpCjs7ICAgICAgKHNldHEgY29udGFpbmVyLW5hbWUgIiIpKQo7Owo7OyAgKHVubGVzcyAoc3RyaW5ncCBjb250YWluZXItbmFtZSkKOzsgICAgKHNpZ25hbCAndHlwZS1lcnJvcgo7OyAgICAgICAgICAgIChmb3JtYXQgImBjb250YWluZXItbmFtZScgc2hvdWxkIGJlIGEgc3RyaW5nIG5vdCAlczogJXMiCjs7ICAgICAgICAgICAgICAgICAgICAodHlwZS1vZiBjb250YWluZXItbmFtZSkKOzsgICAgICAgICAgICAgICAgICAgIGNvbnRhaW5lci1uYW1lKSkpCjs7Cjs7ICAobGV0ICgoc3RhcnQgIiIpIChlbmQgIiIpIChuZXh0LWRpc3BsYXktbGV2ZWwgKCsgMSBkaXNwbGF5LWxldmVsKSkpCjs7ICAgICh3aGVuIChsZW5ndGg+IGNvbnRhaW5lci1uYW1lIDApCjs7ICAgICAgKHNldHEgc3RhcnQgKGZvcm1hdCAiOzsgPGxpc3QgbmFtZT1cIiVzXCI+IiBjb250YWluZXItbmFtZSkKOzsgICAgICAgICAgICBlbmQgKGZvcm1hdCAiOzsgPC9saXN0IG5hbWU9XCIlc1wiPiIgY29udGFpbmVyLW5hbWUpKSkKOzsgICAgKG1hcGNhciAjJyhsYW1iZGEgKGl0ZW0pCjs7ICAgICAgICAgICAgICAgIChsZXQqICgKOzsgICAgICAgICAgICAgICAgICAgICAgIChpdGVtLXR5ICh0eXBlLW9mIGl0ZW0pKQo7OyAgICAgICAgICAgICAgICAgICAgICAgKQo7OyAgICAgICAgICAgICAgICAgIChjb25kICgoc3RyaW5ncCBpdGVtKQo7OyAgICAgICAgICAgICAgICAgICAgICAgICAoZm9ybWF0ICIlUyIgaXRlbSkpCjs7Cjs7ICAgICAgICAgICAgICAgICAgICAgICAgKChudW1iZXJwIGl0ZW0pCjs7ICAgICAgICAgICAgICAgICAgICAgICAgIChmb3JtYXQgIiVkIiBpdGVtKSkKOzsKOzsgICAgICAgICAgICAgICAgICAgICAgICAoKGxpc3RwIGl0ZW0pCjs7ICAgICAgICAgICAgICAgICAgICAgICAgIChkaXNwbGF5LWxpc3QgaXRlbSBuZXh0LWRpc3BsYXktbGV2ZWwpKQo7Owo7OyAgICAgICAgICAgICAgICAgICAgICAgICh0Cjs7ICAgICAgICAgICAgICAgICAgICAgICAgIChwcm9nbgo7OyAgICAgICAgICAgICAgICAgICAgICAgICAgIChtZXNzYWdlICInKGRpc3BsYXktbGlzdCkgd2FybmluZzogdW5rbm93biB0eXBlICVTIGZvciBpdGVtICVTIiBpdGVtLXR5IGl0ZW0pCjs7ICAgICAgICAgICAgICAgICAgICAgICAgICAgKGZvcm1hdCAiJVMgOzsgdHlwZSAlUyIgaXRlbSBpdGVtLXR5KSkpCjs7ICAgICAgICAgICAgICAgICAgICAgICAgKSA7OyBlbmQgY29uZAo7OyAgICAgICAgICAgICAgICAgICkgOzsgZW5kIGxldCoKOzsgICAgICAgICAgICAgICAgKSA7OyBlbmQgbGFtYmRhCjs7ICAgICAgICAgICAgY29udGFpbmVyLXZhbHVlcwo7OyAgICAgICAgICAgICkgOzsgZW5kIG1hcGNhcgo7OyAgICApIDs7IGVuZCBsZXQKOzsgICkgOzsgZW5kIGRlZnVuIGRpc3BsYXktbGlzdAo7Owo=
+
 (defun uncollapse-repeated-open-or-close-parenthesis(string)
   (let* (
          (regexp "\\([()]\\)\\(\\1\\)+")
          (result string)
          (rounds -1)
-         (states nil)
+         (states (list))
          (state-count 0)
          );; end let* varlist
-    (erase-c-messages)
-    (c-message-open "(uncollapse-repeated-open-or-close-parenthesis %S)" string)
+
     (save-match-data
       (string-match regexp result)
       (while (string-match regexp result)
@@ -17,8 +18,10 @@
                 (extra-occurrences (match-string 2 result))
                 (whole-match (match-string 0 result))
                 (new-text (uncollapse-repeated-open-or-close-parenthesis--explode-levels parens-char extra-occurrences))
+                (states-list (pp-to-string states))
                 ) ;; end let varlist
-            (c-message-debug-symbols (list 'result 'new-text 'whole-match)  'state-count 'string 'parens-char 'extra-occurrences)
+            (c-message-debug-symbols (list 'result 'states-list 'new-text 'whole-match)
+                                     'state-count 'string 'parens-char 'extra-occurrences)
             (setq result (replace-match new-text nil nil result))
             (setq states (append states (list result)))
             (setq state-count (length states))
@@ -62,4 +65,16 @@
           (format "%s%s%s\n" sol padding-left parens-char)))
         levels))))
 
-(uncollapse-repeated-open-or-close-parenthesis "()((())())")
+(progn
+  (enable-debug-on-error)
+  (erase-c-messages)
+  (let* (
+         (input-string "()((())())")
+         groupies)
+    (setq groupies
+          (uncollapse-repeated-open-or-close-parenthesis
+           input-string)
+          )
+
+    (c-message "<groups>\n%s\n</groups>\n" groupies)
+    ))
