8079ca70ffdb24670dd3e20ae0df37c7388d0b13
Gabriel Falcão <gabrielteratos@gmail.com>
2026-01-29 06:10:06 +0000

    saves resolution-predicate-for-symbol-by-name.el

---
+++ c/resolution-predicate-for-symbol-by-name.el
@@ -0,0 +1,30 @@
+(defmacro try-or-nil (s-exprs)
+  `(condition-case err
+       (cons ,s-exprs nil)
+     (error
+      (let (message (format
+                     "%s trying to execute expression %S"
+                     (error-message-string err)
+                     s-exprs))
+        (cons nil message)))))
+
+  `(if (eq ,variable t)
+       (setq ,variable nil)))
+
+(let* (
+       (sym 'error)
+       (name (symbol-name sym))
+       (value (symbol-value sym))
+       (props (symbol-plist sym))
+       (func (symbol-function sym))
+       (is-function (not (null func)))
+       )
+  (erase-c-messages)
+  (c-message-open "debugging symbol `%s'" sym)
+  (c-message-debug-symbols (list 'sym
+                                 'name
+                                 'value
+                                 'props
+                                 'func
+                                 )
+                           'sym))
