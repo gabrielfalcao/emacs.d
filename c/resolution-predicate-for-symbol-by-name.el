337cbba5830296db7bd015c1e1673d3b9f484701
Gabriel Falcão <gabrielteratos@gmail.com>
2026-01-29 06:11:02 +0000

    saves resolution-predicate-for-symbol-by-name.el

--- c/resolution-predicate-for-symbol-by-name.el
+++ c/resolution-predicate-for-symbol-by-name.el
@@ -7,14 +7,10 @@
                      (error-message-string err)
                      s-exprs))
         (cons nil message)))))
-
-  `(if (eq ,variable t)
-       (setq ,variable nil)))
-
 (let* (
        (sym 'error)
-       (name (symbol-name sym))
-       (value (symbol-value sym))
+       ;; (name (symbol-name sym))
+       ;; (value (symbol-value sym))
        (props (symbol-plist sym))
        (func (symbol-function sym))
        (is-function (not (null func)))
@@ -22,8 +18,8 @@
   (erase-c-messages)
   (c-message-open "debugging symbol `%s'" sym)
   (c-message-debug-symbols (list 'sym
-                                 'name
-                                 'value
+                                 ;; 'name
+                                 ;; 'value
                                  'props
                                  'func
                                  )
