24a2ff63a87d9b6b161619218e25636d00f697a1
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-18 20:45:33 +0000

    saves c-staging-after-save-hooks.el

---
+++ c/c-staging-after-save-hooks.el
@@ -0,0 +1,10 @@
+(defun rust-delete-cargo-lock-after-saving-cargo-toml()
+  (let* (
+         (buf-filename-full-path (buffer-file-name))
+         (filename (file-name-nondirectory buf-filename-full-path))
+         (filename-base (file-name-base filename))
+         (filename-ext (file-name-extension filename-base))
+         )
+    )
+  )
+(add-hook 'after-save-hook 'rust-delete-cargo-lock-after-saving-cargo-toml )
