86dc99af85f84e35f6ee64d22bc1aa066b4580d9
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-20 09:17:34 +0000

    save status

--- c/c-staging-after-save-hooks.el
+++ c/c-staging-after-save-hooks.el
@@ -2,9 +2,12 @@
   (let* (
          (buf-filename-full-path (buffer-file-name))
          (filename (file-name-nondirectory buf-filename-full-path))
-         (filename-base (file-name-base filename))
+         (filename-base (progn
+                          (unless (
+                          (file-name-base filename)))
          (filename-ext (file-name-extension filename-base))
          )
     )
   )
+
 (add-hook 'after-save-hook 'rust-delete-cargo-lock-after-saving-cargo-toml )
