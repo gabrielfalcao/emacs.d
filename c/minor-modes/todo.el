43ebc17797d6f52094bae4a173bf115a75f0ddba
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-22 20:32:35 +0000

    quickly save state: new files

---
+++ c/minor-modes/todo.el
@@ -0,0 +1,9 @@
+(let ((tmppotbuf (make-potbuf
+                  :path "/Applications/Emacs.app/Contents/Resources/lisp/emacs-lisp/easy-mmode.el.gz"
+                  :line 149
+                  :col 0
+                  :pos 6253
+                  )
+(define-minor-mode (potbuf-open
+  "Toggle the tool bar in all graphical frames (Tool Bar mode)."
+  )
