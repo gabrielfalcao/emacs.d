779d74ec96bfb99eb7aba607b6bfc0eda583fe8a
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-23 13:07:39 +0000

    saves keys.el

--- c/keys.el
+++ c/keys.el
@@ -65,6 +65,7 @@
 
 ;; (Ox33b4O/$/set-key '("C-x M-t") #')
 (Ox33b4O/$/set-key '("C-x C-a") #'eval-elisp-buffer)
+(Ox33b4O/$/set-key '("C-x C-x") #'eval-elisp-buffer)
 
 (Ox33b4O/$/set-key '("C-x C--") 'text-scale-adjust)
 (Ox33b4O/$/set-key '("C-x C-+") 'text-scale-adjust)
