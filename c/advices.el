07f1ac85c0fc67678fd1879b388a93f65bde8162
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-24 03:32:37 +0000

    c/advices.el

--- c/advices.el
+++ c/advices.el
@@ -6,3 +6,27 @@
 ;;                          (read-buffer "Find file: "
 ;;                                       (existing-file-current-buffer)
 ;;                                       (null current-prefix-arg))))))
+(defun advise-replace-regexp-before-call (regexp to-string &rest args)
+  ;; (ignore-errors
+  ;;   (erase-c-messages)
+  ;;   (c-message-open "" ))
+  (setq case-fold-search nil)
+  (setq-default case-fold-search nil)
+  ;; (c-message "replace-regexp called with REGEXP `%S' and TO-STRING `%S'" regexp to-string)
+  (message "isearch-forward-regexp called with args: `%S'" args)
+  )
+
+(advice-add #'replace-regexp :before #'advise-replace-regexp-before-call)
+
+
+(defun advise-isearch-forward-regexp-before-call (&rest args)
+  ;; (ignore-errors
+  ;;   (erase-c-messages)
+  ;;   (c-message-open "" ))
+  ;; (c-message "isearch-forward-regexp called with REGEXP `%S' and TO-STRING `%S'" regexp to-string)
+  (setq case-fold-search t)
+  (setq-default case-fold-search t)
+  (message "isearch-forward-regexp called with args: `%S'" args)
+  )
+
+(advice-add #'isearch-forward-regexp :before #'advise-isearch-forward-regexp-before-call)
