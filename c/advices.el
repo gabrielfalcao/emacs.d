6548732aea4df430130e2c61bbb39c48d769bc17
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-26 00:31:21 +0000

    saves advices.el

--- c/advices.el
+++ c/advices.el
@@ -1,8 +1,8 @@
-(defadvice switch-to-buffer (before existing-file activate compile)
-  "when interactive, try to auto-complete to existing file first."
-  (interactive
-   (list
-    (find-file-read-args "Find file: "
-                         (read-buffer "Find file: "
-                                      (existing-file-current-buffer)
-                                      (null current-prefix-arg))))))
+;; (defadvice switch-to-buffer (before existing-file activate compile)
+;;   "when interactive, try to auto-complete to existing file first."
+;;   (interactive
+;;    (list
+;;     (find-file-read-args "Find file: "
+;;                          (read-buffer "Find file: "
+;;                                       (existing-file-current-buffer)
+;;                                       (null current-prefix-arg))))))
