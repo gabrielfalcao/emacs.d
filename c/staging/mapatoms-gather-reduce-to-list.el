d7f312b7689008bf70dfbc09feaa2dcd020ce463
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-28 20:43:00 +0000

    saves mapatoms-gather-reduce-to-list.el

--- c/staging/mapatoms-gather-reduce-to-list.el
+++ c/staging/mapatoms-gather-reduce-to-list.el
@@ -1,7 +1,5 @@
 (defvar all-atoms-list (list))
-(defvar all-atoms-hash (make-hash-table ))
-
-(defvar gathering-count 0)
+;; (defvar all-atoms-hash (make-hash-table))
 (defvar gathering-index 0)
 
 (defun for-each-atom (val)
@@ -11,10 +9,23 @@
          (pos            (+ gathering-index 1))
          (id             (format "atom@%d" pos))
          (name           (cond ((symbolp val) (symbol-name val))
-                               (            t (format "atom@%d#%s" pos ty))
-                               ))
+                               (            t (format "unnamed#%s@%d" ty pod))))
+         (name-and-id    (string-join (list (format "%s-%d" ty index) name ) ":::"))
+         (repr           (format "%S" val))
          )
     (setq gathering-index (1+ gathering-index))
+    (let ((atom-info (list
+                      :ty ty
+                      :index index
+                      :pos pos
+                      :id id
+                      :name name
+                      :name-and-id name-and-id
+                      :repr repr
+                      )))
+      (push atom-info all-atoms-list)
+
+    )
     )
   )
 (mapatoms #'for-each-atom)
