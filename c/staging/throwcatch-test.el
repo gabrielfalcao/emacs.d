4fc5b17ac7cb5d3e4c75e830e9305f53e251778c
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-28 20:06:48 +0000

    saves throwcatch-test.el

--- c/staging/throwcatch-test.el
+++ c/staging/throwcatch-test.el
@@ -1,32 +1,40 @@
-(progn
+(enable-debug-on-error)
+(let ((final-result
+       (let* ((teens (mapcar (lambda (n) (* 10 n)) (number-sequence 1 9)))
+              (caught
+               (catch 'numba
+                 (mapc
+                  (lambda (lownu)
+                    (mapc
+                     (lambda (factor)
+                       (let (
+                             (value (+ lownu factor))
+                             (return)
+                             )
+                         (setq return
+                               (list :lownu lownu :factor factor :val
+                                     (* lownu factor)))
+                         (when (or
+                                (= value 37)
+                                (= value 30))
+                           (throw 'numba return))
+                         )
+                       )
+                     teens)
+                    )
+                  (number-sequence 1 40))
+                 )
+               )
+              )
+         caught
+         )
+       )
+      )
+
   (erase-c-messages)
   (c-message-open "")
-  (condition-case waterr
-      (let* ((teens (mapcar (lambda (n) (* 10 n)) (number-sequence 1 9)))
-             (caught
-              (catch 'numba
-                (mapc
-                 (lambda (lownu)
-                   (mapc
-                    (lambda (factor)
-                      (let (
-                            (value (+ lownu factor))
-                            (return)
-                            )
-                        (setq return
-                              (list :lownu lownu :factor factor :val
-                                    (* lownu factor)))
-                        (when (or
-                               (= value 37)
-                               (= value 30))
-                          (throw 'numba return))
-                        )
-                      )
-                    teens)
-                   )
-                 (number-sequence 2 25))
-                ))
-             (c-message "caught: (%S) %S" (type-of caught) caught))
-        )
-    )
-  )
+  (c-message
+   "final-result: (%S) %S"
+   (type-of final-result)
+   final-result)
+)
