ffdc84e746139ef53758e29d34851a76e70bff4d
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-28 20:52:36 +0000

    saves mapatoms-gather-reduce-to-list.el

--- c/staging/mapatoms-gather-reduce-to-list.el
+++ c/staging/mapatoms-gather-reduce-to-list.el
@@ -1,5 +1,12 @@
 (let (
       (all-atoms-list (list))
+      (all-strings-list (list))
+      (all-symbols-list (list))
+      (all-functions-list (list))
+      (all-vectors-list (list))
+      (all-hash-tables-list (list))
+      (all-lists-list (list))
+      (all-sequences-list (list))
       (gathering-index 0)
       )
 
@@ -15,19 +22,27 @@
 		       (repr           (format "%S" val))
 		       )
 		  (setq gathering-index (1+ gathering-index))
-		  (let ((atom-info (list
-				    :ty ty
-				    :index index
-				    :pos pos
-				    :id id
-				    :name name
-				    :name-and-id name-and-id
-				    :repr repr
-				    )))
-		    (push atom-info all-atoms-list)
-
+		  (let ((info (list
+			       :ty ty
+			       :index index
+			       :pos pos
+			       :id id
+			       :name name
+			       :name-and-id name-and-id
+			       :repr repr
+			       )))
+		    (push info all-atoms-list)
+                    (cond nil
+                          ((functionp val)      (push info all-functions-list))
+                          ((symbolp val)        (push info  all-symbols-list))
+                          ((stringp val)        (push info  all-strings-list))
+                          ((vectorp val)        (push info  all-vectors-list))
+                          ((hash-table-p val)   (push info  all-hash-tables-list))
+                          ((listp val)          (push info  all-lists-list))
+                          ((sequencep val)      (push info  all-sequences-list))
+			  )
 		    )
-		  )
+                  )
 		)
-            )
+	    )
   )
