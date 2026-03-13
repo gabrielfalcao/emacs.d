1b08cfdbbbe60d17f64748137154d94972a37dc4
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-13 18:27:52 +0000

    saves save-buffer-list-wip.el

--- c/staging/save-buffer-list-wip.el
+++ c/staging/save-buffer-list-wip.el
@@ -1,6 +1,11 @@
 (defun save-open-buffers-as-wip-in-workbench ()
   (interactive)
-  (let* (
+  (let* ((epoch (string-to-number (format-time-string "%s")))
+         (today (format-time-string "%Y-%m-%d" epoch t))
+         (date-YYYY (format-time-string "%Y" epoch t))
+         (date-MM (format-time-string "%m" epoch t))
+         (date-DD (format-time-string "%d" epoch t))
+
          (json-false "false")
          (json-null "null")
          (json-object-type 'plist)
@@ -8,65 +13,81 @@
          (json-key-type 'string)
 
          (workbench-root (file-name-canonicalize "~/workbench"))
-         (today (format-time-string "%Y-%m-%d" nil t))
+
+
          (workbench-path (file-name-concat workbench-root today))
-         (workbench-wip-buffers-path (file-name-concat workbench-path
-                                                       (format "emacs-buffers.%s" (emacs-pid))))
+         (workbench-wip-buffers-path
+          (file-name-concat workbench-path
+                            (format "emacs-buffers")
+                            (format "emacs-buffers-%s" date-YYYY)
+                            (format "emacs-buffers-%s-%s" date-YYYY date-MM)
+                            (format "emacs-buffers-%s-%s-%s" date-YYYY date-MM date-DD)
+                            (format "emacs-buffers.pid-%s" (emacs-pid))))
          (workbench-wip-buffers-index-file-path
           (progn
             (mkdir workbench-wip-buffers-path t)
-            (file-name-concat workbench-wip-buffers-path
-                              "index.json")))
+            (file-name-concat workbench-wip-buffers-path "index.json")))
          (buffers (buffer-list))
          (buffer-count (length buffers))
          (index-file-json-array-items
           (seq-map-indexed
            (lambda (buf index)
              (with-current-buffer buf
-               (let* (
-		      (number (1+ index))
+               (let* ((number (1+ index))
 		      (pos (format "%d of %d" number buffer-count))
 		      (buffer-name-raw (buffer-name buf))
 		      (filename (buffer-file-name buf))
 		      (is-file (not (null filename)))
-		      (buffer-name-fallback (string-to-kebab buffer-name-raw))
-		      (wip-filename (format "%s.bkp" (string-to-kebab (or filename buffer-name-raw))))
-                      (wip-full-path (file-name-concat workbench-wip-buffers-path wip-filename))
-		      (bufname (if is-file (file-name-nondirectory filename)  buffer-name-fallback))
-		      (bufcontents (save-match-data (save-mark-and-excursion (widen) (buffer-substring-no-properties (point-min) (point-max)))))
+		      (buffer-name-fallback
+                       (string-to-kebab buffer-name-raw))
+		      (wip-filename
+                       (format "%s.bkp"
+                               (string-to-kebab
+                                (or filename buffer-name-raw))))
+                      (wip-full-path
+                       (file-name-concat workbench-wip-buffers-path wip-filename))
+		      (bufname
+                       (if is-file
+                           (file-name-nondirectory filename)
+                         buffer-name-fallback))
+		      (bufcontents
+                       (save-match-data
+                         (save-mark-and-excursion
+                           (widen)
+                           (buffer-substring-no-properties
+                            (point-min)
+                            (point-max)))))
 		      (buf-plist-to-json-object
                        (list
-			:index          index
-			:number         number
-			:total          buffer-count
-			:name           bufname
-			:filename       filename
-			:wip_filename   wip-filename
-			:pos            pos
-			:name_raw       buffer-name-raw
-			:is_file        (if is-file "true" "false")
-			:contents       bufcontents
-			:size           (length bufcontents)
-			))
-                      ) ;; end (with-current-buffer (let* ...) varlist )
+		        :index          index
+		        :number         number
+		        :total          buffer-count
+		        :name           bufname
+		        :filename       filename
+		        :wip_filename   wip-filename
+		        :pos            pos
+		        :name_raw       buffer-name-raw
+		        :is_file (if is-file "true" "false")
+		        :contents       bufcontents
+		        :size (length bufcontents)
+		        ))) ;; end (with-current-buffer (let* ...) varlist )
                  ;; <seq-map-indexed lambda body>
                  (write-region bufcontents         nil wip-full-path nil nil nil nil)
                  ;; </seq-map-indexed lambda body>
                  );; end (let* ...)
                );; end (with-current-buffer ...)
              ) ;; end (lambda (buf index)
-           buffers
-           ) ;; end (seq-map-indexed
+           buffers) ;; end (seq-map-indexed
           )
-         (index-file-contents (json-encode index-file-json-array-items))
-         )
+         (index-file-contents
+          (json-encode index-file-json-array-items)))
     ;; <defun body>
 
     ;; <write index file>
     (condition-case err
         (write-region index-file-contents nil workbench-wip-buffers-index-file-path nil nil nil nil)
       (error
-       (user-error "failed to write index file to %S: %s" workbench-wip-buffers-index-file-path err) ))
+       (user-error "failed to write index file to %S: %s" workbench-wip-buffers-index-file-path err)))
     ;; </write index file>
     );; end (defun ... (let* ... ))
   );; end (defun save-open-buffers-as-wip-in-workbench)
