3489d344190ed7a42ddfd0b518eabf019fc6209c
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-20 10:57:48 +0000

    saves debug-fonts.el

---
+++ c/staging/debug-regexp-subexpressions.el
@@ -0,0 +1,95 @@
+(defun get-debug-info-regexp-subexpressions ()
+  "this function extracts useful debug information from `match-data',
+returns plist whose keys are `keyword' objects and values are either
+global match-data info such as the count of subespressions found in the
+current match-data, or per-match information such as the boundaries of
+each match.
+
+"
+  (let* (
+         (md (match-data))
+         (md-len (length md))
+         (total-pairs (/ md-len 2))
+         (total-subexps (- total-pairs 1))
+         (subexp-seq (number-sequence 0 total-subexps))
+         (subexps (seq-map-indexed
+                   (lambda (num index)
+                     (let* (
+                            (beginning (match-beginning num))
+                            (end       (match-end num))
+                            (string    (match-string num))
+                            )
+                       (list
+                        :index      index
+                        :num        num
+                        :beginning (match-beginning num)
+                        :end       (match-end num)
+                        :string    (match-string num)
+                        ))
+                     )
+                   subexp-seq))
+         ) ; end (let* (...varlist...))
+    (list :subexps subexps
+          :pairs total-pairs
+          :total-subexps total-subexps
+          :match-data md
+          :match-data-info  ;; end (let* ...)
+          )
+    ) ;; end (defun get-debug-info-regexp-subexpressions ...)
+  )
+
+
+(defun debug-regexp-subexpressions (&optional collapse-linebreaks)
+  "this function returns a string with N+1 lines, where every line is
+comprised of the subexp number and subexp contents for each subexp in
+the current `match-data' in order to help debug and visualize matched
+groups in interactive invocations `replace-regexp' involving relatively
+complex regular expressions containing multiple groups.
+
+this function is meant to be used as the `TO-STRING' argument of the
+interactive command `replace-regexp' like so:
+
+\,(debug-regexp-subexpressions)
+"
+  (let ((regexp-groups
+         (mapcar
+          (lambda (g)
+            (let ((subexp-start (format "%d" g)) ;; (subexp-start (format "<%d>" g))
+                  (value-prefix   "=`")
+                  (value (match-string-no-properties g))
+                  (value-suffix   "`")
+                  (subexp-end     "")              ;; (subexp-end (format "</%d>" g))
+                  (item-separator "")) ;; end let varlist
+              (when (stringp value)
+                (let ((items
+                       (list
+                        subexp-start
+                        value-prefix
+                        (if collapse-linebreaks ;; if `cond' argument #0
+                            (save-match-data
+                              (replace-regexp-in-string
+                               "\\(\\s-*\\)\\(\r\n\\|\n\\)+\\(\\s-*\\)" "\\1\\3" value)) ;; if `then' argument #1
+                          value  ;; if `else' argument #2
+                          )
+                        value-suffix
+                        subexp-end)))
+                  ;; (message "items %S" items)
+                  ;; (c-message-open "items %S" items)
+                  (string-join items item-separator) ;; (format "%d=`%s`\n" g value)
+                  ))) ;
+            )         ;; mapcar `function' argument #0
+          (number-sequence 0
+                           (- (/ (length (match-data)) 2) 1))  ;; mapcar `sequence' argument #1
+          ) ;; string-join `strings' argument   #0
+         ))
+    (format "\n%s\n\n" (string-join regexp-groups "\n" ;; string-join `separator' argument #1
+                                    )))
+
+  ) ;; end defun debug-regexp-subexpressions
+(defalias '~dbg-regex #'debug-regexp-subexpressions)
+(defalias '~dbg-regexp #'debug-regexp-subexpressions)
+(defalias 'dbg-regex #'debug-regexp-subexpressions)
+(defalias 'dbg-regexp #'debug-regexp-subexpressions)
+(defalias 'regex! #'debug-regexp-subexpressions)
+
+;; 2026/03/20 07:16:33 (1773990993)
--- init.el
+++ init.el
@@ -6,11 +6,33 @@
 
 (defalias 'describe #'describe-symbol)
 
+(defun safe-load-file (file-name)
+  (condition-case err
+      (load-file file-name)
+    (error
+     (display-warning 'emacs
+                      (format "failed to load-file `%s': %s" file-name err)
+                      ; :debug / :warning / :error / :emergency
+                      :emergency
+                      )
+     ); end (display-warning ...)
+    )
+  )
+
 (defun safe-load-library (library-name)
   (condition-case err
       (load-library library-name)
     (error
-     (message "failed to load-library `%s':\n%s" library-name err))))
+          (display-warning 'emacs
+                           (format "failed to load-library `%s':\n%s" library-name err)
+                      ; :debug / :warning / :error / :emergency
+                      :emergency
+                      )
+          )
+    )
+  )
+
+          
 
 (defun safe-load (file &rest load-optional-args)
   (condition-case err
@@ -57,48 +79,7 @@
   (add-to-list 'load-path "~/.emacs.d/c")
   (safe-load-library "boot")
   (safe-load-library "elfmt"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(expand-region-guess-python-mode nil)
- '(ignored-local-variable-values '((sh-indent-comment . t)))
- '(package-selected-packages
-   '(ansi autothemer blacken brightscript-mode cargo-mode company
-          company-box csharp-mode dockerfile-mode elixir-mode
-          exec-path-from-shell expand-region flycheck-rust
-          flymake-shellcheck gdscript-mode go-mode haml-mode
-          highlight-indentation jinja2-mode jsonnet-mode kotlin-mode
-          lua-mode markdown-mode nginx-mode pcre2el peg php-mode
-          protobuf-mode python-mode pythonic restclient rust-mode
-          sed-mode solidity-flycheck swift-mode terraform-mode toml
-          toml-mode typescript-mode web-mode yaml-mode))
- '(py-electric-colon-greedy-p t)
- '(py-electric-colon-newline-and-indent-p t)
- '(py-indent-honors-inline-comment t)
- '(py-indent-no-completion-p t)
- '(py-ipython-command "~/.shell.d/.venv/bin/ipython3")
- '(py-known-shells '("~/.shell.d/.venv/bin/python3"))
- '(py-known-shells-extended-commands '("~/.shell.d/.venv/bin/python3"))
- '(py-max-specpdl-size 1337)
- '(py-python-command "~/.shell.d/.venv/bin/python")
- '(py-python3-command "~/.shell.d/.venv/bin/python3")
- '(py-pythonpath "~/.shell.d/.venv/lib/python3.12/site-packages")
- '(py-tab-indent nil)
- '(py-tab-indents-region-p t)
- '(py-tab-shifts-region-p t)
- '(python-indent-offset 4 t)
- '(python-interpreter "~/.shell.d/.venv/bin/python3")
- '(python-mode-hook
-   '(#[nil
-       ((define-key python-mode-map (kbd "C-c C-f")
-                    #'g/format/prettify))
-       nil nil nil nil]))
- '(ruby-deep-indent-paren '(40 91 93 t 58))
- '(ruby-indent-level 0)
- '(ruby-method-call-indent nil)
- '(ruby-method-params-indent 4))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -109,9 +90,6 @@
 ;;(delete-minibuffer-contents)
 (kill-buffer "*Messages*")
 (line-number-mode #x594553)
-(progn
-  (autoload 'disable-bars "~/.emacs.d/c/boot.el")
-  (disable-bars))
 (set-frame-parameter nil 'fullscreen 'maximized)
 (setq-default debug-on-error nil)
 (put 'downcase-region 'disabled nil)
