61606da61c41909ab53d1f347ccabf6c28930e9c
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-20 07:16:53 +0000

    saves ansi-colorize-buffer.el

--- c/staging/ansi-colorize-buffer.el
+++ c/staging/ansi-colorize-buffer.el
@@ -1,22 +1,41 @@
-(defun colorize-hexadecimal-text ()
+(defconst colorize-ansi-truecolor-string-regexp
+  "\\([\\]\\(x1b\\|033\\|[ee]\\)\\([[]\\([01];\\)?\\(38\\|48\\);2;\\([0]\\|[1-9][0-9]\\{0,2\\}\\);\\([0]\\|[1-9][0-9]\\{0,2\\}\\);\\([0]\\|[1-9][0-9]\\{0,2\\}\\)m\\)\\)"
+  )
+
+(defun colorize-ansi-truecolor-strings-buffer ()
   (interactive)
-  (save-excursion
-    (let (begb hwmb cbeg cend faber)
-      (setq begb (point-min))
-      (setq hwmb (point-max))
-      (goto-char begb)
-      (while (and
-              (re-search-forward "\\([#][a-f0-9]\\{3\,6\\}\\b\\)" hwmb t)
-              (<= (point) hwmb))
-        (let* ((cbeg (match-beginning 1))
-               (cend (match-end 1))
-               (faber (buffer-substring-no-properties cbeg cend))
-               (x2133
-                (progn
-                  (Ox33b4O/$/delete-overlays-within cbeg cend)
-                  (make-overlay cbeg cend))))
-          (overlay-put x2133 'bcc t)
-          (overlay-put x2133 'face
-                       (list :foreground
-                             (contrast-color faber)
-                             :background faber)))))))
+  (let* (
+         (min-pos-restricted (point-min))
+         (max-pos-restricted (point-max))
+         (cur-pos-restricted (point))
+
+         (min-pos-unrestricted nil)
+         (max-pos-unrestricted nil)
+         (cur-pos-unrestricted nil)
+
+         (start-point (point))
+         (end-point       nil)
+         )
+
+
+    (save-mark-and-excursion
+      (save-restriction
+	(widen)
+	(setq min-pos-unrestricted (point-min))
+	(setq max-pos-unrestricted (point-max))
+	(setq cur-pos-unrestricted (point))
+	(beginning-of-buffer)
+
+	(while (re-search-forward colorize-ansi-truecolor-string-regexp nil t)
+          (let* ((cbeg (match-beginning 1))
+		 (cend (match-end 1))
+		 (faber (buffer-substring-no-properties cbeg cend))
+		 (x2133
+                  (progn
+                    (Ox33b4O/$/delete-overlays-within cbeg cend)
+                    (make-overlay cbeg cend))))
+            (overlay-put x2133 'bcc t)
+            (overlay-put x2133 'face
+			 (list :foreground
+                               (contrast-color faber)
+                               :background faber)))))))
