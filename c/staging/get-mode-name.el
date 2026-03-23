67136b8c6761e7d81b64f664ab54fa1ddb41c2ff
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-23 05:31:54 +0000

    saves get-mode-name.el

---
+++ c/staging/get-mode-name.el
@@ -0,0 +1,35 @@
+(defun get-mode-name-neat-only-string(&optional buffer-or-name)
+  (let* (
+         (buf (cond
+
+               ((bufferp buffer-or-name)
+                (get-buffer buffer-or-name))
+
+               ((and (stringp buffer-or-name) (get-buffer buffer-or-name))
+                (get-buffer buffer-or-name))
+
+               ((null buffer-or-name) (current-buffer))
+
+               (t
+                (signal 'type-error (format  "argument `buffer-or-name' must be either a `buffer', `string' with existing buffer name or `nil' but instead received a `%s': %S"
+                                             (cl-type-of buffer-or-name)
+                                             buffer-or-name))
+                )
+               ))
+
+         (strings (seq-filter (lambda (s) (and (stringp s) (not (string-match-p "[^a-zA-Z0-9_-]" s )))) (flatten-tree mode-name)))
+         )
+    (when (seq-empty-p strings)
+      (signal 'error (format  "failed to obtain `mode-name' for buffer \"%s\"" (buffer-name buf))))
+
+    (car strings))
+  )
+
+(defun get-mode-name(&optional buffer-or-name)
+  (downcase
+   (substring-no-properties
+    (format "%s-mode"
+            (get-mode-name-neat-only-string buffer-or-name))
+    )
+   )
+  )
