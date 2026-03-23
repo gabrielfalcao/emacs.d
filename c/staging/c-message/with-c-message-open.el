c8dd124eda98d5975486d4da75222d8362e61e0d
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-23 04:22:01 +0000

    saves with-c-message-open.el

--- c/staging/c-message/with-c-message-open.el
+++ c/staging/c-message/with-c-message-open.el
@@ -1,5 +1,28 @@
 (defmacro with-c-message-open (&optional window-position &rest body)
-  `(progn ,@body))
+  (let* ((orig-body (copy-sequence body))
+         (window-location
+          (pcase window-position
+            ((or "right" :right 'right)
+             #'split-window-right)
+            ((or "left" :left 'left)
+             #'split-window-horizontally)
+            ((or "above" :above 'above)
+             #'split-window-vertically)
+            ((or "below" :below 'below)
+             #'split-window-below)
+            (_
+             (setq body `(progn ,window-position ,@body))
+             (c-message "<body type=%S>\n%S\n</body>"
+                        (format "%S" (cl-type-of body))
+                        body)))
+          )
+         (existing-buffer (get-buffer c-message-buffer))
+         (existing-window
+          (and  existing-buffer (get-buffer-window existing-buffer)))
+         (current-windows (window-list nil :minibuf-never)))
+
+    `(progn ,@body))
+  )
 
 
 (progn
@@ -11,3 +34,13 @@
               `(with-c-message-open
                 (erase-c-messages)
                 (c-message "foo")))))
+
+
+
+;; (when (eq (frame-first-window) existing-window)
+;;       (delete-window existing-window))
+
+;;     (delete-other-windows (frame-first-window))
+;;     (set-window-buffer
+;;      (funcall window-location)
+;;      (get-buffer-create c-message-buffer))
