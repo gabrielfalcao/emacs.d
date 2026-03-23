9f3b42708939df2cd200a39b8e9b034c5061f9af
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-23 14:42:33 +0000

    saves emacs-lisp-search-defs.el

--- c/staging/emacs-lisp-search-defs.el
+++ c/staging/emacs-lisp-search-defs.el
@@ -1,24 +1,26 @@
-;;;defadvice
-;;;defalias
-;;;defclass
-;;;defconst
-;;;defconst-mode-local
-;;;defcustom
-;;;defcustom-c-stylevar
-;;;defface
-;;;defgroup
-;;;defimage
-;;;defmacro
-;;;defmath
-;;;defsubst
-;;;deftheme
-;;;defun
-;;;defun-declarations-alist
-;;;defun-markdown-buffer
-;;;defun-markdown-ref-checker
-;;;defun-prompt-regexp
-;;;defvar
-;;;defvar-keymap
-;;;defvar-local
-;;;defvar-mode-local
-;;;defvaralias
+(defconst emacs-lisp-defsymbols
+  '('defadvice
+       'defalias
+       'defclass
+     'defconst
+     'defconst-mode-local
+     'defcustom
+     'defcustom-c-stylevar
+     'defface
+     'defgroup
+     'defimage
+     'defmacro
+     'defmath
+     'defsubst
+     'deftheme
+     'defun
+     'defun-declarations-alist
+     'defun-markdown-buffer
+     'defun-markdown-ref-checker
+     'defun-prompt-regexp
+     'defvar
+     'defvar-keymap
+     'defvar-local
+     'defvar-mode-local
+     'defvaralias)
+  "list of symbols which define types in emacs lisp")
