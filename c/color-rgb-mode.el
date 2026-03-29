b92984430b99a4e521dffb035c16b9f0c62a7dc7
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-29 02:16:13 +0000

    poc minor-mode color-rgb-mode.el

---
+++ c/color-rgb-mode.el
@@ -0,0 +1,22 @@
+(defvar color-rgb-mode nil
+  "Mode variable for color-rgb mode"
+  (make-variable-buffer-local 'color-rgb-mode))
+
+
+(defun color-rgb-mode (&optional arg)
+  "color-rgb minor mode."
+  (interactive "P")
+  (setq color-rgb-mode
+        (if (null arg)
+            (not color-rgb-mode)
+          (>= (prefix-numeric-value arg) 0)))
+
+  (if color-rgb-mode
+      (color-rgb-minor-mode-enable)
+    (color-rgb-minor-mode-disable)))
+
+(defun color-rgb-minor-mode-enable()
+  (add-hook 'after-change-functions #'color-rgb-colorize-buffer nil t))
+
+(defun color-rgb-minor-mode-disable()
+  (remove-hook 'after-change-functions #'color-rgb-colorize-buffer t))
