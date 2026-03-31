4e26403add3c177bd2b6d4d7543c8f6d0f02f1d6
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-31 05:35:24 +0000

    c-message-visible-p.el

--- c/staging/c-message/c-message-visible-p.el
+++ c/staging/c-message/c-message-visible-p.el
@@ -1,15 +1,65 @@
-(defun c-message-visible-p ()
+(defun get-frame-params (item-frame)
+  (let* (;
+         (params-list (frame-parameters item-frame))
+         (params-count (length params-list))
+         (params-plists
+          (seq-map-indexed
+           (lambda (item-param-cons param-index)
+             (let* (;
+                    (item-no (1+  param-index))
+                    (key (car item-param-cons))
+                    (value (cdr item-param-cons))
+                    ;;
+                    )
+               (list :key key
+                     :value value
+                     :type (cl-type-of value)
+                     :index param-index
+                     :num   item-no
+                     :total params-count)
+               ))
+           params-plists)
+          )
+         )
+    params-plists
+    )
+  )
+
+
+(defun get-frame-params-plist (item-frame)
+  (apply #'append
+  (mapcar (lambda (frame-each-param-plist)
+            (let* (
+                   (key   (plist-get frame-each-param-plist :key))
+                   (value (plist-get frame-each-param-plist :value))
+                   )
+              (list key value)
+              )
+            )
+          (get-frame-params item-frame))
+  )
+  )
+
+(defun c-message-debug-frame (item-frame)
   (let* (
-         ;;
-         (vis-frames                (visible-frame-list))
-         (total-items               (length vis-frames))
-         (item-frame                 (nth 0 vis-frames))
+                                        ;
+         (params (get-frame-params item-frame))
+         ;
+         )
+    (c-message "frame %S params: %S" item-frame (get-frame-params-plist item-frame))
+    )
+  )
+
+(defun c-message-visible-p ()
+  (let* (;;
+         (vis-frames (visible-frame-list))
+         (total-items (length vis-frames))
+         (item-frame (nth 0 vis-frames))
          ;;
          )
     (c-message-open)
     (erase-c-messages)
-
-    (c-message "frame: %S" item-frame)
+    (c-message-debug-frame item-frame)
     ;; (seq-do-indexed (lambda (item-frame item-index)
     ;;                   (let* (
     ;;                          ;;
@@ -22,5 +72,5 @@
     ;;                   )
     ;;                 vis-frames
     ;;                 )
-    )
-  )
+    ))
+(c-message-visible-p)
