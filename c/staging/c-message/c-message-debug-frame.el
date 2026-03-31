1d87a77e7d569595a8016d5bb24288f58893f7ee
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-31 14:13:22 +0000

    WIP/draft `c-message'-related funs

---
+++ c/staging/c-message/c-message-debug-frame.el
@@ -0,0 +1,7 @@
+(defun c-message-debug-frame (item-frame)
+  (let* (;
+         (params (get-frame-params item-frame))
+         ;;
+         )
+    (c-message "frame %S params: %S" item-frame
+               (get-frame-params-plist item-frame))))
---
+++ c/staging/c-message/get-frame-params-plist.el
@@ -0,0 +1,8 @@
+(defun get-frame-params-plist (item-frame)
+  (apply #'append
+         (mapcar
+          (lambda (frame-each-param-plist)
+            (let* ((key (plist-get frame-each-param-plist :key))
+                   (value (plist-get frame-each-param-plist :value)))
+              (list key value)))
+          (get-frame-params item-frame))))
