e675368afc3660b73562ac99f7e6828356fe61af
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-31 20:36:21 +0000

    saves c-message-visible-p.el

--- c/staging/c-message/c-message-visible-p.el
+++ c/staging/c-message/c-message-visible-p.el
@@ -29,10 +29,10 @@
                    (item-value (funcall indent-part (format "%S" item)))
                    (parts (list item-start item-value item-end))
                    (items (mapcar indent-part parts)))
-              (string-join items item-separator))
-            )))
-    (string-join (seq-map-indexed on-each-item-indexed items) item-separator))
-  )
+              (string-join items item-separator)))))
+    (string-join
+     (seq-map-indexed on-each-item-indexed items)
+     item-separator)))
 
 
 
@@ -46,33 +46,41 @@ must be *live* (.i.e.: must pass both predicates `framep' and
 "
   (let* (
 
-         (vis-windows              (window-list frame))
-         (vis-buffers              (mapcar #'window-buffer vis-windows))
-         (vis-buffer-names         (seq-filter #'stringp (mapcar #'buffer-name  vis-buffers)))
-         (c-message-buf-membership (member c-messages-buffer-name vis-buffer-names))
-         (result                   (and (listp c-message-buf-membership) (length> c-message-buf-membership 0)))
-         )
+         (vis-windows (window-list frame))
+         (vis-buffers (mapcar #'window-buffer vis-windows))
+         (vis-buffer-names
+          (seq-filter #'stringp (mapcar #'buffer-name  vis-buffers)))
+         (c-message-buf-membership
+          (member c-messages-buffer-name vis-buffer-names))
+         (result
+          (and
+           (listp c-message-buf-membership)
+           (length> c-message-buf-membership 0))))
 
     (when debug-internals
-      (let* (
-             (vars-dbg (list (--c-message-list-to-debug-string vis-windows              "vis-windows")
-                             (--c-message-list-to-debug-string vis-buffers              "vis-buffers")
-                             (--c-message-list-to-debug-string vis-buffer-names         "vis-buffer-names")
-                             (--c-message-list-to-debug-string c-message-buf-membership "c-message-buf-membership")
-                             (format "<result type={%s}>\n%S\n</result>" (cl-type-of result) result) ))
-             (do-debug (lambda (&rest extra-dbg-items)
-                         (c-message "%s" (string-join (append extra-dbg-items vars-dbg)  "\n"))))
-             )
-        (apply do-debug (list :result                   result
-                              :c-message-buf-membership c-message-buf-membership))))
-    result
-    )
+      (let* ((vars-dbg
+              (list
+               (--c-message-list-to-debug-string vis-windows              "vis-windows")
+               (--c-message-list-to-debug-string vis-buffers              "vis-buffers")
+               (--c-message-list-to-debug-string vis-buffer-names         "vis-buffer-names")
+               (--c-message-list-to-debug-string c-message-buf-membership "c-message-buf-membership")
+               (format "<result type={%s}>\n%S\n</result>"
+                       (cl-type-of result)
+                       result)))
+             (do-debug
+              (lambda (&rest extra-dbg-items)
+                (c-message "%s"
+                           (string-join
+                            (append extra-dbg-items vars-dbg)
+                            "\n")))))
+        (apply do-debug
+               (list :result                   result
+                     :c-message-buf-membership c-message-buf-membership))))
+    result)
   )
 
 (defun --c-message-visible-p-debug(&optional frame)
-  (let* (
-         (result nil)
-         )
+  (let* ((result nil))
     (c-message-open)
     (erase-c-messages)
     (c-message "<c-message-visible-p>")
@@ -83,5 +91,4 @@ must be *live* (.i.e.: must pass both predicates `framep' and
     (c-message "%S" result)
     (c-message "</result>")
 
-    )
-)
+    ))
