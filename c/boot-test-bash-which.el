378f23ac59e91b60a97c177b6aecfb3923057d25
Gabriel Falcão <gabrielteratos@gmail.com>
2026-04-01 02:19:31 +0000

    boot-test-bash-which.el

---
+++ c/boot-test-bash-which.el
@@ -0,0 +1,91 @@
+(defun repr-value (value) (format "%S" item))
+
+(defun make-indent-function (&optional level indentation-string)
+  (let* ((default-level                4)
+         (default-indentation-string " ")
+         (level (or (and (natnump level) level) default-level))
+         (indentation-string
+          (or
+           (and
+            (stringp indentation-string)
+            (length> indentation-string 0))
+           default-indentation-string)))
+    (lambda (value)
+      (let* ((indentation
+              (string-join (make-list level indentation-string) "")))
+        (format "%s%s" indentation value)))))
+
+
+(defun list-items-to-string-values (items)
+  (unless (or (listp items) (consp items))
+    (signal 'type-error
+            (format "argument ITEMS is not a list `%s': %S"
+                    (cl-type-of items)
+                    items))
+    )
+  (mapcar #'repr-value items))
+
+
+(defun indented-list-items (items)
+  (string-join
+   (mapcar
+    (make-indent-function 4 " ")
+    (list-items-to-string-values items))
+   "\n"))
+
+
+
+(defun which-bin (name)
+  "Returns a list of all executable files and symlinks in all of the paths returned by `exec-path' matching NAME.
+
+   Every symbolic link, if any, is resolved to the canonical path of its
+   source and added to the beginning of the list returned by this
+   function, but is *NOT* replaced.
+
+   The return value is a `list' of valid filesystem paths.
+"
+  (let* ((trim-regexp "[ \t\n\r[:space:]]+")
+         (which-a-bash (shell-command-to-string "which -a bash"))
+         (which-a-lines
+          (save-match-data
+            (split-string which-a-bash "\n" t trim-regexp)))
+         (canonical-paths
+          (mapcar
+           (lambda (path)
+             (string-trim
+              (shell-command-to-string
+               (format "path canon %S" (string-trim path)))))
+           which-a-lines))
+         (result (list)))
+    (seq-do-indexed
+     (lambda (item idx)
+       (unless (member item result)
+         (when (and (stringp item) (file-exists-p item))
+           (push item result))))
+     (append  which-a-lines canonical-paths))
+
+    (unless (c-message-visible-p)
+      (c-message-open)
+      (erase-c-messages))
+    (erase-c-messages)
+    (erase-all-non-file-buffers)
+    ;; (c-message "which-a-bash: %S" which-a-bash)
+    (c-message "which-a-lines:\n%s"
+               (string-join
+                (mapcar
+                 (lambda (val) (format "%S" val))
+                 which-a-lines)
+                "\n"))
+    (c-message "canonical-paths: %s"
+               (string-join
+                (mapcar
+                 (lambda (val) (format "%S" val))
+                 canonical-paths)
+                "\n"))
+    (c-message "result: %s"
+               (string-join
+                (mapcar (lambda (val) (format "%S" val)) result)
+                "\n"))))
+
+
+(which-bin "bash")
