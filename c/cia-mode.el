8e21e9e05435187c4e2aa8f300712a6ffc6fde7a
Gabriel Falcão <gabrielteratos@gmail.com>
2026-04-05 20:43:30 +0000

    cia-mode.el

---
+++ c/cia-mode.el
@@ -0,0 +1,16 @@
+;;; cia-mode.el --- Special Features; -*- lexical-binding: t -*-
+;; Author: Gabriel DeMoura <gabrielteratos@gmail.com>
+;; URL: ...TK
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "30.1"))
+;;
+;; Copyright (C) 2024-2026 Gabriel DeMoura
+;;
+;;; Commentary:
+;;
+;; Special Mode
+;;
+;;; Code:
+
+
+(provide 'cia-mode)
