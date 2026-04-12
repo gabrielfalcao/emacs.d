8a631bf8148e4b795001aa6cfac8321026992640
Gabriel Falcão <gabrielteratos@gmail.com>
2026-04-12 14:07:12 +0000

    v/text-mode-ietf.el

---
+++ v/text-mode-ietf.el
@@ -0,0 +1,6 @@
+;;
+;; ;; ;; ;; (defface ietf-txt-toc-title '((t :inherit font-lock-string-face))
+;; ;; ;; ;;   "Table of Contents"
+;; ;; ;; ;;   :group 'text-mode
+;; ;; ;; ;;   :version "22.1")
+;;
