00db0d68808035a0c5c36559ab00c7c31b50a900
Gabriel Falcão <gabrielteratos@gmail.com>
2026-04-22 21:28:34 +0000

    saves install.el

--- install.el
+++ install.el
@@ -6,62 +6,80 @@
                                               "ansi"
                                               "autothemer"
                                               "blacken"
+                                              "brightscript-mode"
                                               "cargo-mode"
                                               "company"
+                                              "company-box"
                                               "csharp-mode"
+                                              "dash"
                                               "dockerfile-mode"
                                               "elixir-mode"
+                                              ;; epl
                                               "exec-path-from-shell"
                                               "expand-region"
+                                              ;; f
+                                              "flycheck"
                                               "flycheck-rust"
+                                              "flymake-shellcheck"
+                                              "frame-local"
+                                              "fringe-helper"
+                                              "gdscript-mode"
                                               "go-mode"
                                               "haml-mode"
+                                              "hcl-mode"
                                               "highlight-indentation"
+                                              ;; ht
                                               "jinja2-mode"
                                               "jsonnet-mode"
+                                              "kotlin-mode"
+                                              "line-reminder"
                                               "lua-mode"
                                               "markdown-mode"
                                               "nginx-mode"
+                                              ;; ov
                                               "pcre2el"
-                                              "peg"
                                               "php-mode"
+                                              "pkg-info"
                                               "protobuf-mode"
                                               "python-mode"
+                                              "pythonic"
                                               "restclient"
                                               "rust-mode"
+                                              ;; s
+                                              "sed-mode"
                                               "solidity-flycheck"
+                                              "solidity-mode"
                                               "swift-mode"
-                                              "systemd"
                                               "terraform-mode"
                                               "toml"
                                               "toml-mode"
                                               "typescript-mode"
                                               "web-mode"
                                               "yaml-mode"
-                                              "yasnippet"
-                                              "yasnippet-snippets"))
+
+                                              ))
          (already-installed (list))
          (install-succeeded (list))
          (install-failed (list)))
     (mapc (lambda (name)
-           (if (package-installed-p name)
-             (push name already-installed)
-             (condition-case err
-               (progn
-                 (package-install name)
-                 (push name install-succeeded))
-               (error
+            (if (package-installed-p name)
+                (push name already-installed)
+              (condition-case err
+                  (progn
+                    (package-install name)
+                    (push name install-succeeded))
+                (error
                  (push name (list 'package-name install-failed 'installation-error err))))))
-      packages-to-install-if-not-already)
+          packages-to-install-if-not-already)
     (message "<package-installation-result>\n\n%s\n\n<package-installation-result>"
-      (string-join (list
-                    (format "<failed>\n%s\n</failed>\n" (string-join (mapcar (lambda (props)
-                                                                              (let* (
-                                                                                     (package-name (plist-get 'package-name props))
-                                                                                     (error-object (plist-get 'installation-error props)))
-                                                                                (format "    %S: %s" package-name (error-message-string error-object))))
-                                                                      install-failed)
-                                                         "\n"))
-                    (format "<succeeded>\n%s\n</succeeded>\n" (string-join install-succeeded "\n"))
-                    (format "<already-installed-previously>\n%s\n</already-installed-previously>\n" (string-join install-succeeded "\n")))
-        "\n"))))
+             (string-join (list
+                           (format "<failed>\n%s\n</failed>\n" (string-join (mapcar (lambda (props)
+                                                                                      (let* (
+                                                                                             (package-name (plist-get 'package-name props))
+                                                                                             (error-object (plist-get 'installation-error props)))
+                                                                                        (format "    %S: %s" package-name (error-message-string error-object))))
+                                                                                    install-failed)
+                                                                            "\n"))
+                           (format "<succeeded>\n%s\n</succeeded>\n" (string-join install-succeeded "\n"))
+                           (format "<already-installed-previously>\n%s\n</already-installed-previously>\n" (string-join install-succeeded "\n")))
+                          "\n"))))
